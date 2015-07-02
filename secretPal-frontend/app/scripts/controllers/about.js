00cca0cee9d5286e83a0318b8de3ea34a226d11f
Roman Rizzi <roman.rizzi@10pines.com>
2015-07-02 17:25:35 -0300

    Adding frontend project scaffold

---
+++ secretPal-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name secretPalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the secretPalApp
+ */
+angular.module('secretPalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ secretPal-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name secretPalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the secretPalApp
+ */
+angular.module('secretPalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
