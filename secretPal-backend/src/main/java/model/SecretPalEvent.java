07abf953088312d0d6ecf20a7cc0fcac7273bc40
Roman Rizzi <roman.rizzi@10pines.com>
2015-07-07 12:33:13 -0300

    tested added and hasAnyParticipants method in SecretPalEvent

--- secretPal-backend/src/main/java/model/SecretPalEvent.java
+++ secretPal-backend/src/main/java/model/SecretPalEvent.java
@@ -1,9 +1,21 @@
 package model;
 
 
+import java.util.List;
+
 public class SecretPalEvent {
 
+    private List<Person> participants;
+
+    public SecretPalEvent(List<Person> participants){
+        this.participants = participants;
+    }
+
     public boolean hasAnyParticipant() {
-        return false;
+        return !this.participants.isEmpty();
+    }
+
+    public void registerParticipant(Person aPerson) {
+        this.participants.add(aPerson);
     }
 }
--- secretPal-backend/src/test/java/model/SecretPalEventTest.java
+++ secretPal-backend/src/test/java/model/SecretPalEventTest.java
@@ -1,6 +1,10 @@
 package model;
 
+import builder.PersonBuilder;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 
@@ -8,8 +12,16 @@ public class SecretPalEventTest {
 
     @Test
     public void When_I_create_a_new_secret_pal_event_it_has_no_participants() {
-        SecretPalEvent aSecretPalEvent = new SecretPalEvent();
+        SecretPalEvent aSecretPalEvent = new SecretPalEvent(new ArrayList<>());
         assertFalse(aSecretPalEvent.hasAnyParticipant());
     }
 
+    @Test
+    public void When_I_add_a_participant_to_a_secret_pal_event_it_has_participants(){
+        SecretPalEvent aSecretPalEvent = new SecretPalEvent(new ArrayList<>());
+        Person aRandomPerson = new PersonBuilder().build();
+        aSecretPalEvent.registerParticipant(aRandomPerson);
+
+        assertTrue(aSecretPalEvent.hasAnyParticipant());
+    }
 }
