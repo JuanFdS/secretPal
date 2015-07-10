430fd6608265c4b781b048815d7103544e8377de
victoriacabrera <toiacabrera@hotmail.com>
2015-07-10 17:00:57 -0300

    controller for the nav-bar

--- secretPal-frontend/app/scripts/app.js
+++ secretPal-frontend/app/scripts/app.js
@@ -28,4 +28,11 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+
+  .controller('navCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.navClass = function (page) {
+      var currentRoute = $location.path().substring(1) || 'home';
+      return page === currentRoute ? 'active' : '';
+    };
+  }]);
---
+++ secretPal-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name secretPalApp.controller:WorkersCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the secretPalApp
+ */
+var app = angular.module('secretPalApp');
+
+/*
+app.controller('mainCtrl', function ($scope, $location) {
+}
+*/
