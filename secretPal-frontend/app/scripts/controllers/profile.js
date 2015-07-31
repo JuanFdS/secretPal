7c813aa848b776b643bb86305da732f414b44d92
Roman Rizzi <roman.rizzi@10pines.com>
2015-07-31 15:06:44 -0300

    Friend page unavailable for non participants

--- secretPal-frontend/app/scripts/controllers/profile.js
+++ secretPal-frontend/app/scripts/controllers/profile.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('secretPalApp')
-  .controller('ProfileController', function($scope, user, FriendRelationService, WishlistService) {
+  .controller('ProfileController', function($scope, user, $location, FriendRelationService, WishlistService) {
 
     FriendRelationService.getFriend(user.data.worker, function(friend){
         $scope.friend = friend;
+
+        if(friend.data == ""){
+          $location.path('/');
+          alert("No estas participando, avisale al administrador");
+        }
+
           WishlistService.getAllWishesFor($scope.friend.data, function(wishlist){
               $scope.wishlist = wishlist;
           });
