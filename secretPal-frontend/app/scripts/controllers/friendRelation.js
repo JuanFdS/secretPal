9ba99d73c3766f849ee24b4855e50c20b9437ea4
victoriacabrera <toiacabrera@hotmail.com>
2015-08-10 09:13:09 -0300

    making add_relations action collpased

--- secretPal-frontend/app/scripts/controllers/friendRelation.js
+++ secretPal-frontend/app/scripts/controllers/friendRelation.js
@@ -1,9 +1,18 @@
 'use strict';
 
 var app = angular.module('secretPalApp');
-app.controller('FriendRelationController', function($scope, $modal, $filter, FriendRelationService) {
+app.controller('FriendRelationController', function($scope, $modal, $filter, FriendRelationService, SweetAlert) {
 
   FriendRelationService.all( function(data) {$scope.friendRelations = data;});
+  $scope.relations = $filter('filter')($scope.friendRelations, {giftReceiver:null});
+
+  function warningMsg(msg){
+    SweetAlert.swal({
+      title: "",
+      text: msg,
+      type: "warning"
+    });
+  }
 
   $scope.deleteRelation = function (relation) {
     FriendRelationService.delete(relation.giftGiver.id, relation.giftReceiver.id, function() {
@@ -15,11 +24,41 @@ app.controller('FriendRelationController', function($scope, $modal, $filter, Fri
     return (relation.giftReceiver !== null);
   };
 
-  $scope.openModalForAssign = function() {
+  $scope.notUsed = function(giftGiverSelected){
+    return function (relation){
+      var notUsed = true;
+      angular.forEach($scope.friendRelations, function(fr){
+        if (fr.giftReceiver !== null) {
+          if (fr.giftReceiver.id === relation.giftGiver.id) {
+            if (fr.giftGiver.id !== giftGiverSelected) {notUsed = false;}
+          }
+        }
+      });
+      return notUsed;
+    };
+  };
+
+  $scope.ok = function () {
+    angular.forEach($scope.relations, function(relation) {
+        if (relation.giftReceiver !== null) {
+          FriendRelationService.new(relation.giftGiver.id, relation.giftReceiver.id, function() { $scope.error = true; });}}
+    );
+
+    if (!$scope.error) {
+      $scope.cancel();
+    }
+  };
+
+  $scope.cancel = function () {
+    /*$scope.reset();
+    descollapsed*/
+  };
+
+  /*$scope.openModalForAssign = function() {
     var modalInstance = $modal.open({
       animation: false,
       templateUrl: '../../views/addFriendRelationModal.html',
-      controller: 'pal_assignmentCtrl',
+      controller: 'addRelationsController',
       resolve: {
         relationsXXX: function () {
           return angular.copy($scope.friendRelations);
@@ -29,11 +68,11 @@ app.controller('FriendRelationController', function($scope, $modal, $filter, Fri
     modalInstance.result.then(function () {
       location.reload();
     });
-  };
+  };*/
 
 });
 
-app.controller('pal_assignmentCtrl', function ($scope, $modalInstance, $filter, FriendRelationService, relationsXXX) {
+app.controller('addRelationsController', function ($scope, $modalInstance, $filter, FriendRelationService, relationsXXX) {
 
   $scope.friendRelations = relationsXXX;
   $scope.relations = $filter('filter')($scope.friendRelations, {giftReceiver:null});
