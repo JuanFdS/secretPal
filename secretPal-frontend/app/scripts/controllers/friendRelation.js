b75eed2ac517fcf968ae0d2ae4ea2cb0c4a1fbf3
jazcarate <joaquin.azcarate@10pines.com>
2015-08-13 17:45:59 -0300

    One month to it

--- secretPal-frontend/app/scripts/controllers/friendRelation.js
+++ secretPal-frontend/app/scripts/controllers/friendRelation.js
@@ -3,9 +3,23 @@
 var app = angular.module('secretPalApp');
 app.controller('FriendRelationController', function($scope, $modal, $filter, FriendRelationService, SweetAlert) {
 
+  $scope.thisMonth = new Date().getMonth();
+
+  $scope.hasBirthdayInAMonth = function(worker){
+      return new Date(worker.dateOfBirth).getMonth() <= $scope.thisMonth;
+  };
+
   FriendRelationService.all( function(data) {
     $scope.friendRelations = data;
-    $scope.posibilities = $scope.friendRelations.map( function(relation){ return relation.giftGiver; });
+    $scope.posibilities = $scope.friendRelations.map( function(relation){
+      return relation.giftGiver;
+    });
+
+    /*.filter(function(worker){
+      return !$scope.hasBirthdayInAMonth(worker);
+    });*/
+
+    console.log($scope.posibilities);
 
     $scope.relations = $filter('filter')($scope.friendRelations, {giftReceiver:null});
   });
@@ -75,55 +89,4 @@ var shuffleArray = function(array) {
   }
 
   return array;
-}
-
-/*app.controller('addRelationsController', function ($scope, $modalInstance, $filter, FriendRelationService, relationsXXX) {
-
-  $scope.friendRelations = relationsXXX;
-  $scope.relations = $filter('filter')($scope.friendRelations, {giftReceiver:null});
-
-  $scope.notUsed = function(giftGiverSelected){
-    return function (relation){
-      var notUsed = true;
-      angular.forEach($scope.friendRelations, function(fr){
-        if (fr.giftReceiver !== null) {
-          if (fr.giftReceiver.id === relation.giftGiver.id) {
-            if (fr.giftGiver.id !== giftGiverSelected) {notUsed = false;}
-          }
-        }
-      });
-      return notUsed;
-    };
-  };
-
-  $scope.ok = function () {
-    angular.forEach($scope.relations, function(relation) {
-        if (relation.giftReceiver !== null) {
-          FriendRelationService.new(relation.giftGiver.id, relation.giftReceiver.id, function() { $scope.error = true; });}}
-    );
-
-    if (!$scope.error) {
-      $modalInstance.close();
-    }
-  };
-
-  $scope.auto = function(){
-    angular.forEach($scope.relations, function(relation) {
-      if(relation.giftReceiver === null) {
-
-        var arr = $filter('filter')($scope.friendRelations, $scope.notUsed(relation.giftGiver.id));
-        arr = $filter('filter')(arr, function (who) {
-          return who.giftGiver !== relation.giftGiver;
-        });
-
-        var whatYouWant = arr[Math.floor(Math.random() * arr.length)].giftGiver;
-
-        relation.giftReceiver = whatYouWant;
-      }
-    })
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-});*/
+};
