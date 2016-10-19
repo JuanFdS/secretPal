6adab855a5fb485197efa0399d108ad42b943a35
kevin <kevenvarela@gmail.com>
2016-10-19 11:34:55 -0300

    nuevos admins y puesta en marcha

--- secretPal-backend/src/main/java/com/tenPines/model/InitializerLocalSystem.java
+++ secretPal-backend/src/main/java/com/tenPines/model/InitializerLocalSystem.java
@@ -6,7 +6,8 @@ import com.tenPines.application.service.WorkerService;
 import java.time.LocalDate;
 
 public class InitializerLocalSystem {
-    public  Worker joaco = new Worker("joaco", "joaquin.azcarate@10pines.com", LocalDate.now(), true);
+    public  Worker kevin = new Worker("kevin", "kevin.varela@10pines.com", LocalDate.of(1993,4,6), true);
+    public  Worker aye = new Worker("aye", "ayelen.garcia@10pines.com", LocalDate.of(1992,3,24), true);
     private WorkerService workerService;
 
     public InitializerLocalSystem(WorkerService workerService) {
@@ -16,6 +17,7 @@ public class InitializerLocalSystem {
 
     void init() {
         workerService.removeAll();
-        workerService.save(joaco);
+        workerService.save(kevin);
+        workerService.save(aye);
     }
 }
