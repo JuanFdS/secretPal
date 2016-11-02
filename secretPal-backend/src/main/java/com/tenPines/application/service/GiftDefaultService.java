8456ddfd37d3cd329541120717ebd344297d2420
ayelenMacarena <ayelenmacarenagarcia@gmail.com>
2016-11-02 16:09:27 -0300

    Test de controller de backEnd, y mockeado de front

--- secretPal-backend/src/main/java/com/tenPines/application/service/GiftDefaultService.java
+++ secretPal-backend/src/main/java/com/tenPines/application/service/GiftDefaultService.java
@@ -1,6 +1,11 @@
 package com.tenPines.application.service;
 
+import com.tenPines.model.GiftDefault;
+import com.tenPines.persistence.GiftDefaultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Aye on 02/11/16.
@@ -8,4 +13,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GiftDefaultService {
+
+    @Autowired
+    GiftDefaultRepository repository;
+
+    public List<GiftDefault> getAll() {
+        return repository.findAll();
+
+    }
+
 }
--- secretPal-backend/src/test/java/com/tenPines/integration/FacadeTest.java
+++ secretPal-backend/src/test/java/com/tenPines/integration/FacadeTest.java
@@ -2,12 +2,27 @@ package com.tenPines.integration;
 
 
 import com.tenPines.application.SystemPalFacade;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class FacadeTest {
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+public class FacadeTest extends SpringBaseTest {
 
     @Autowired
     SystemPalFacade facade;
+
+    @Test
+    public void when_i_get_all_gift_defaults_and_is_empty(){
+        assertThat(facade.retrieveAllGiftsDefaults(), empty());
+    }
+
+    @Test
+    public void when_i_get_all_gift_defaults_and_have_anything(){
+       assertThat(facade.retrieveAllGiftsDefaults(), empty());
+    }
 
 
 }
