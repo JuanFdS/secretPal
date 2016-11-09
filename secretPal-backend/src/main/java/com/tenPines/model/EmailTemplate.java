f7ceafe7a1143cc043b55738b1d14ca78796acab
ayelenMacarena <ayelenmacarenagarcia@gmail.com>
2016-11-09 10:13:26 -0300

    Persistiendo template de mail

---
+++ secretPal-backend/src/main/java/com/tenPines/model/EmailTemplate.java
@@ -0,0 +1,90 @@
+package com.tenPines.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+/**
+ * Created by Aye on 09/11/16.
+ */
+@Entity
+public class EmailTemplate {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    String subject;
+
+    String bodyText;
+
+    LocalDate dateOfBirth;
+
+    String fullName;
+
+    Boolean active;
+
+
+    public EmailTemplate() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBodyText() {
+        return bodyText;
+    }
+
+    public void setBodyText(String bodyText) {
+        this.bodyText = bodyText;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+
+    public void modifiedTemplate(EmailTemplate modifiedMail) {
+        this.setActive(modifiedMail.getActive());
+        this.setDateOfBirth(modifiedMail.getDateOfBirth());
+        this.setFullName(modifiedMail.getFullName());
+        this.setBodyText(modifiedMail.getBodyText());
+        this.setSubject(modifiedMail.getSubject());
+    }
+
+}
